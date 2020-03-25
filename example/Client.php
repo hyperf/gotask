1d63c351be04466944f25f050a681b56b062350d
Reasno <guxi99@gmail.com>
2020-03-25 17:16:01 +0800

    fix outdated example

--- example/Client.php
+++ example/Client.php
@@ -1,13 +1,13 @@
 <?php
 
-use Reasno\GoTask\GoTask;
 use Reasno\GoTask\Relay\CoroutineSocketRelay;
+use Spiral\Goridge\RPC;
 use function Swoole\Coroutine\run;
 
 require_once "../vendor/autoload.php";
 
 run(function(){
-    $task = new GoTask(
+    $task = new RPC(
         new CoroutineSocketRelay("127.0.0.1", 6001)
     );
     var_dump($task->call("App.Hi", "Antony"));
