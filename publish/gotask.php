c540af66fcf36b62cdbc97e617fa16827e8bbb4d
Reasno <guxi99@gmail.com>
2020-03-31 11:44:17 +0800

    fix go build command

--- publish/gotask.php
+++ publish/gotask.php
@@ -18,12 +18,18 @@ return [
         'enable' => false,
         'address' => \Reasno\GoTask\ConfigProvider::address(),
     ],
+    'go_build' => [
+        'enable' => false,
+        'command' => sprintf(
+            'go build -o %1$s/bin/app %1$s/gotask/cmd/app.go', BASE_PATH
+        ),
+    ],
     'pool' => [
         'min_connections' => 1,
         'max_connections' => 30,
         'connect_timeout' => 10.0,
         'wait_timeout' => 30.0,
         'heartbeat' => -1,
-        'max_idle_time' => (float) env('GOTASK_MAX_IDLE_TIME', 60),
+        'max_idle_time' => (float)env('GOTASK_MAX_IDLE_TIME', 60),
     ],
 ];
