afaecbac4354fdd83773d625c64b16254a8df14d
slairmy <Slairmy@163.com>
2022-03-04 12:38:59 +0800

    增加bulkWrite实际插入数据的数量 (#51)
    * Update server.go
    
    * 修复bulkWrite异常
    
    * 回退格式化的代码
    
    Co-authored-by: 丁少龙 <dingshaolong@aipai.com>

--- src/MongoClient/Type/BulkWriteResult.php
+++ src/MongoClient/Type/BulkWriteResult.php
@@ -37,6 +37,11 @@ class BulkWriteResult implements Unserializable
     private $deletedCount;
 
     /**
+     * @var int
+     */
+    private $insertedCount;
+
+    /**
      * @var array<ObjectId>
      */
     private $upsertedIds;
@@ -47,6 +52,7 @@ class BulkWriteResult implements Unserializable
         $this->modifiedCount = $data['modifiedcount'];
         $this->upsertedCount = $data['upsertedcount'];
         $this->deletedCount = $data['deletedcount'];
+        $this->insertedCount = $data['insertedcount'];
         $this->upsertedIds = (array) $data['upsertedids'];
     }
 
@@ -74,4 +80,10 @@ class BulkWriteResult implements Unserializable
     {
         return (array) $this->upsertedIds;
     }
+
+    public function getinsertedCount(): int
+    {
+        return $this->insertedCount;
+    }
+
 }
